ed3081294a5d7e01c29ec08fa4ff3cf55c22d673
“reddrum” <anton.kranz@me.com>
2016-09-09 18:32:42 +0300

    Add title scrape

--- scraper.rb
+++ scraper.rb
@@ -2,10 +2,15 @@
 require 'curb'
 require 'nokogiri'
 
-http = Curl.get("http://www.interfax.ru/") do |http|
+http = Curl.get("http://www.nytimes.com/") do |http|
   http.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
 end
 
-puts http.body_str
-
-html
+html = Nokogiri::HTML(http.body_str)
+html.css(".a-column column").each do |node|
+  sections_html = Nokogiri::HTML(node.inner_html)
+  sections_html.css(".first-column-region region dycontent dyMonitor").each do |node|
+    title = node.css(".story-heading").text
+    puts title, "\n\n"
+  end
+end
