f05bfc144389c45f0ddcdc6ba1f6df2fa9268825
“reddrum” <anton.kranz@me.com>
2016-09-10 23:02:58 +0300

    Fix css tags

--- scraper.rb
+++ scraper.rb
@@ -2,15 +2,15 @@
 require 'curb'
 require 'nokogiri'
 
-http = Curl.get("http://www.nytimes.com/") do |http|
+http = Curl.get("http://www.detroitnews.com/") do |http|
   http.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
 end
 
 html = Nokogiri::HTML(http.body_str)
-html.css(".a-column column").each do |node|
+html.css(".detroitnews-headline-grid-primary").each do |node|
   sections_html = Nokogiri::HTML(node.inner_html)
-  sections_html.css(".first-column-region region dycontent dyMonitor").each do |node|
-    title = node.css(".story-heading").text
+  sections_html.css(".hgpm-list-wrap").each do |node|
+    title = node.css(".hgpm-list-hed").text
     puts title, "\n\n"
   end
 end
