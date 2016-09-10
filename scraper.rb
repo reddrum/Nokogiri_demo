6b40d8111b10b61de8ecd4f01d6d8896d3798135
“reddrum” <anton.kranz@me.com>
2016-09-10 23:25:02 +0300

    Add array: title, description

--- scraper.rb
+++ scraper.rb
@@ -6,12 +6,22 @@ http = Curl.get("http://www.detroitnews.com/") do |http|
   http.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
 end
 
+def extract_parts sections_html, css_name
+  parts = []
+  sections_html.css(css_name).each do |node|
+    parts << {
+      :title => node.css(".hgpm-list-hed").text,
+      :description => node.css(".hgpm-back-listview-text").text
+    }
+  end
+  parts
+end
+
 html = Nokogiri::HTML(http.body_str)
 html.css(".detroitnews-headline-grid-primary").each do |node|
   sections_html = Nokogiri::HTML(node.inner_html)
-  sections_html.css(".hgpm-list-wrap").each do |node|
-    title = node.css(".hgpm-list-hed").text
-    description = node.css(".hgpm-back-listview-text").text
-    puts title, "\n", description, "\n\n"
+  texts = extract_parts sections_html, ".hgpm-list-wrap"
+  texts.each do |part|
+    puts part.inspect, "\n\n"
   end
 end
