4e4893eb42b92ed495151934612b10501ba6a470
“reddrum” <anton.kranz@me.com>
2016-10-22 23:45:34 +0300

    Add some tags in scr5

--- scraper5.rb
+++ scraper5.rb
@@ -5,5 +5,20 @@ url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
 
 document = open(url)
 content = document.read
+parsed_content = Nokogiri::HTML(content)
 
-puts content
+parsed_content.css('.content').css('.row').each do |row|
+  title       = row.css('.hdrlnk').inner_text
+  posted_at   = row.css('time').first.attributes["datetime"].value
+  neighb_elem = row.css('.pnr').css('small')
+
+  if neighb_elem.any?
+    neighborhood = neighb_elem.first.inner_text
+  else
+    neighborhood = ''
+  end
+
+  puts "#{title} #{neighborhood}"
+  puts "Posted at #{posted_at}"
+  puts '-------------------------------------------'
+end
