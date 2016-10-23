3e4c8f67149190069f03a8562888957193e488f2
“reddrum” <anton.kranz@me.com>
2016-10-23 23:29:40 +0300

    Add final edit

--- scraper5.rb
+++ scraper5.rb
@@ -1,24 +1,41 @@
 require 'nokogiri'
 require 'open-uri'
 
-url = "https://newyork.craigslist.org/search/cpg?query=developer&is_paid=all"
+cities = ['newyork', 'lasvegas']
+search_terms = ['ruby', 'python']
 
-document = open(url)
-content = document.read
-parsed_content = Nokogiri::HTML(content)
+cities.each do |city|
+  search_terms.each do |search_term|
 
-parsed_content.css('.content').css('.row').each do |row|
-  title       = row.css('.hdrlnk').inner_text
-  posted_at   = row.css('time').first.attributes["datetime"].value
-  neighb_elem = row.css('.pnr').css('small')
+    url = "http://#{city}.craigslist.org/search/cpg?query=#{search_term}&is_paid=all"
 
-  if neighb_elem.any?
-    neighborhood = neighb_elem.first.inner_text
-  else
-    neighborhood = ''
+    document = open(url)
+    content = document.read
+    parsed_content = Nokogiri::HTML(content)
+
+    puts '============================================='
+    puts '-                                           -'
+    puts "-          #{city} - #{search_term}          "
+    puts '-                                           -'
+    puts '============================================='
+
+    parsed_content.css('.content').css('.row').each do |row|
+      title       = row.css('.hdrlnk').first.inner_text
+      link        = row.css('.hdrlnk').first.attributes["href"].value
+      posted_at   = row.css('time').first.attributes["datetime"].value
+      neighb_elem = row.css('.pnr').css('small')
+
+      if neighb_elem.any?
+        neighborhood = neighb_elem.first.inner_text.strip
+      else
+        neighborhood = ''
+      end
+
+      puts "#{title} #{neighborhood}"
+      puts "Link: #{link}"
+      puts "Posted at #{posted_at}"
+      puts '-------------------------------------------'
+    end
   end
-
-  puts "#{title} #{neighborhood}"
-  puts "Posted at #{posted_at}"
-  puts '-------------------------------------------'
 end
+
